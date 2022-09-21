e25ef56c5383a281c8fa660703fffdf900e80c8d
Modhialghanim <112625141+Modhialghanim@users.noreply.github.com>
2022-09-21 23:21:32 +0300

    done

--- script.js
+++ script.js
@@ -4,18 +4,12 @@ let grades = [90, 94, 65, 80, 39, 55, 83, 92, 39, 93, 43, 70, 57, 0]; //المص
 let all_grades_div = document.getElementById("all-grades");
 let random_grade_div = document.getElementById("random-grade");
 /* لا تقم بتعديل الأسطر المكتوبة بالأعلى */
-
-
 /*  الجزء الأول والثاني من التمرين */
 
 grades.pop[13]
 grades.push(99)
 all_grades_div.getElementById("grades").innerHTML = "  ";
 let randomIndex = Math.floor(Math.random() * grades.length)
-
-
-
-
 
 /* الجزء الثالث من التمرين */
 
