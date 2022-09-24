c19c6b3528b9f48717078fa6a4d7d836ebbcf987
Modhialghanim <112625141+Modhialghanim@users.noreply.github.com>
2022-09-25 00:24:55 +0300

    done

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ let grades = [90, 94, 65, 80, 39, 55, 83, 92, 39, 93, 43, 70, 57, 0]; //المص
 let all_grades_div = document.getElementById("all-grades");
 let random_grade_div = document.getElementById("random-grade");
 /* لا تقم بتعديل الأسطر المكتوبة بالأعلى */
+
 /*  الجزء الأول والثاني من التمرين */
 
-grades.pop[13]
-grades.push(99)
-all_grades_div.getElementById("grades").innerHTML = "  ";
-let randomIndex = Math.floor(Math.random() * grades.length)
+grades.pop();
+grades.push(99);
+grades.forEach(element => {
+  all_grades_div.innerHTML += `<span>${element}<span>`
+});
+
+let randomIndex =  Math.floor(Math.random() * grades.length)
+random_grade_div.innerHTML = `<span>#${randomIndex}: ${grades[randomIndex]}<span>`
+
+
 
 /* الجزء الثالث من التمرين */
 
@@ -23,12 +30,13 @@ let course = {
 
 //Change studentsCount,track property here
 course.studentsCount=40
+course.track="web"
 
 
 
 //Console log here
 
-console.log()
+console.log(`Hello my name is modhi , im in (${ course.track})track value from object at (${course.name})`)
 
 //Create student object here
 let student={
